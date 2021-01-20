4b9cc595c0dc1c7912513b79907ab90385776a11
mwdmwd <pmwpmw@protonmail.ch>
2021-01-20 01:34:48 +0100

    Show StartupWindow instead of main on start

--- main.py
+++ main.py
@@ -4,20 +4,15 @@ import sys
 from PySide2 import QtWidgets
 
 from mq_client import MqTreeModel, MqttListener
-from mainwindow import MainWindow
+from startupwindow import StartupWindow
 
 
 def main(argv):
     app = QtWidgets.QApplication(argv)
 
-    mqtt_listener = MqttListener("192.168.1.10")
-    model = MqTreeModel(app, mqtt_listener=mqtt_listener)
-
-    mqtt_listener.connect()
-
-    window = MainWindow(model)
-
+    window = StartupWindow()
     window.show()
+
     app.exec_()
 
 
